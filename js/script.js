e084dab245a0206266394196a51953896b278a12
Alex <ex@gmail.com>
2019-10-17 09:43:43 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+    // Tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,5 +34,60 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+
+    let deadline = '2019-10-17';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours,
+            'days' : days
+        };
+    }
+
+     function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            days.textContent = t.days;
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+            // ??
+
+            if (t.days < 10) {
+                days.textContent = '0' + t.days;
+            }
+
+            /* for (let i = 1; i < t.length; i++) {
+                let zero = "0";
+                if (t[i] < 10) {
+                    t[i] = zero + t[i]; 
+                }
+            } */
+        }
+     }
+
+     setClock('timer', deadline);
 
 });
