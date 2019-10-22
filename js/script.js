d699de0096267e076dbdc22e83ad5f01148e3dc3
Alex <ex@gmail.com>
2019-10-22 12:39:38 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     // Timer
-    let deadline = '2019-10-18';
+    let deadline = '2019-12-18';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -92,17 +92,15 @@ window.addEventListener('DOMContentLoaded', function() {
         close = document.querySelector('.popup-close'),
         description = document.querySelector('.description-btn');
 
-    more.addEventListener('click', function() {
+    const popUp = function () {
         overlay.style.display = 'block';
         this.classList.add('more-spash');
         document.body.style.overflow = 'hidden';
-    });
-    // ---как без дублирования кода?---
-    description.addEventListener('click', function() {
-        overlay.style.display = 'block';
-        this.classList.add('more-spash');
-        document.body.style.overflow = 'hidden';
-    });
+    };
+
+    more.addEventListener('click', popUp);
+
+    description.addEventListener('click', popUp);
 
     close.addEventListener('click', function() {
         overlay.style.display = 'none';
@@ -110,4 +108,59 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     }); 
 
+    // Form + JSON
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.getElementsByClassName('main-form')[0],
+        contact = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+    
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            //request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            // JSON
+            request.setRequestHeader('Content-type','application/json; charset=utf-8');
+    
+            let formData = new FormData(elem);
+            //request.send(formData);
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+            request.send(json);
+    
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+    
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
+    }
+
+    sendForm(form);
+    sendForm(contact);
+
+    
 });
